95e056ec6247d73e6a11299b8c33df98d9920bc3
Pramod Ganachari <pramod.ganachari@bounteous.com>
2026-03-11 11:30:49 +0530

    Implemented cart system with add update remove cart items

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes";
 import productRoutes from "./product.routes";
+import cartRoutes from "./cart.routes";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/products", productRoutes);
+router.use("/cart", cartRoutes);
 
 router.get("/health", (req, res) => {
   res.json({ status: "API running" });
